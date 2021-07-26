27c9095428dad71a61dac4195f49cd17c7e781b6
wonsikgo <wonsickgo@gmail.com>
2021-07-26 18:48:01 +0900

    NodeAPI - 5.module

---
+++ NodeAPI/3-this/app.js
@@ -0,0 +1,24 @@
+function hello() {
+  console.log(this);
+  console.log(this === global);
+}
+
+hello();
+
+class A {
+  constructor(num) {
+    this.num = num;
+  }
+  memberFunction() {
+    console.log('----------class----------');
+    console.log(this);
+    console.log(this === global);
+  }
+}
+
+const a = new A(1);
+a.memberFunction();
+
+console.log('----------class----------');
+console.log(this);
+console.log(this === module.exports);
---
+++ NodeAPI/4-module/app.js
@@ -0,0 +1,8 @@
+const counter = require('./counter.js');
+
+counter.increase();
+counter.increase();
+counter.increase();
+counter.increase();
+
+console.log(counter.getCount());
