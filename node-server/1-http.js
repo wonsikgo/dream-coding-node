4b8f9daa13d145a8069185cabb8d9ca20c863dc8
wonsikgo <wonsickgo@gmail.com>
2021-08-13 23:57:27 +0900

    Node 서버 생성 - HTML컨텐츠 추가

--- node-server/1-http.js
+++ node-server/1-http.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 //const http2 = require('http2'); // https가 적용되기 때문에 개발단계에서는 http사용
 
 console.log(http.STATUS_CODES);
@@ -10,8 +11,18 @@ const server = http.createServer((req, res) => {
   console.log(req.httpVersion);
   console.log(req.method);
   console.log(req.url);
-  res.write('Welcome');
-  res.end();
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream('./html/index.html').pipe(res);
+  } else if (url === '/courses') {
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream('./html/courses.html').pipe(res);
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream('./html/notFound.html').pipe(res);
+  }
+  //res.end();
 });
 
 server.listen(8080);
