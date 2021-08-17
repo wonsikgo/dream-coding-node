4b5ab8a29b4e657e77f2a8857644c8f5c13dd4f9
wonsikgo <wonsickgo@gmail.com>
2021-08-18 00:09:56 +0900

    express res API

--- 6-express/app.js
+++ 6-express/app.js
@@ -5,10 +5,13 @@ const app = express();
 app.get('/sky/:id', (req, res, next) => {
   //console.log(req.path);
   //console.log(req.headers);
-  //   console.log(req.params);
-  //   console.log(req.params.id);
-  //   console.log(req.query);
-  //   console.log(req.query.keyword);
-  res.send('hi');
+  console.log(req.params);
+  console.log(req.params.id);
+  console.log(req.query);
+  console.log(req.query.keyword);
+  //res.send('hi');
+
+  res.setHeader('key', 'value');
+  res.sendStatus(201).send('created');
 });
 app.listen(8080);
