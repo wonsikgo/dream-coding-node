c91cf94a64cdd921c4d2153dd8f62418523b1af0
wonsikgo <wonsickgo@gmail.com>
2021-08-19 23:54:19 +0900

    express 비동기, 동기 에러 처리

--- 6-express/app.js
+++ 6-express/app.js
@@ -2,16 +2,53 @@ import express from 'express';
 
 const app = express();
 
-app.get('/sky/:id', (req, res, next) => {
-  //console.log(req.path);
-  //console.log(req.headers);
-  console.log(req.params);
-  console.log(req.params.id);
-  console.log(req.query);
-  console.log(req.query.keyword);
-  //res.send('hi');
+app.use(express.json());
 
-  res.setHeader('key', 'value');
-  res.sendStatus(201).send('created');
+app.post('/', (req, res, next) => {
+  console.log(req.body);
+});
+
+/* 비동기 요청에대한 에러 핸들링 */
+app.get('/file', (req, res) => {
+  fs.readFile('/file1.txt', (err, data) => {
+    if (err) {
+      res.sendStatus(404);
+    }
+  });
+});
+
+/* 동기 요청에대한 에러 핸들링 */
+app.get('/file1', (req, res) => {
+  try {
+    const data = fs.readFileSync('/file1.txt');
+    res.send(data);
+  } catch (error) {
+    res.sendStatus(404);
+  }
+});
+
+app.get('/file2', (req, res) => {
+  fsAsync
+    .readFile('/file2.txt')
+    .then((data) => res.send(data))
+    .cathc((err) => res.sendStatus(404));
+});
+
+app.get('/file3', async (req, res) => {
+  try {
+    const data = await fsAsync.readFile('/file2.txt');
+    res.send(data);
+  } catch (error) {
+    res.sendStatus(404);
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).end('Not Found');
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).json({message: 'Something went wrong'});
 });
 app.listen(8080);
