1bbd41adf12799adad0af8bfbcbd10edb57b1844
wonsikgo <wonsickgo@gmail.com>
2021-08-25 00:08:58 +0900

    Dwitter 라우터 설정

--- Dwitter-project/server/app.js
+++ Dwitter-project/server/app.js
@@ -3,13 +3,24 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import 'express-async-errors';
+import tweetsRouter from './router/tweets.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(helmat());
+app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
 
-app.us('/tweets', tweetsRouter);
+app.use('/tweets', tweetsRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+
 app.listen(8080);
