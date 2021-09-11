0f50a5bc6e8bdc0a03fbb2720e071acf3a8cf89d
wonsikgo <wonsickgo@gmail.com>
2021-09-12 01:21:58 +0900

    env 환경변수 설정, jwt 환경변수 값으로 변경

--- Dwitter-project/server/middleware/auth.js
+++ Dwitter-project/server/middleware/auth.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import {config} from '../config.js';
 import * as userRepository from '../data/auth.js';
 
 const AUTH_ERROR = {message: 'Authentication Error'};
@@ -11,7 +12,7 @@ export const isAuth = async (req, res, next) => {
 
   const token = authHeader.split(' ')[1];
   // TODO: Make it secure!
-  jwt.verify(token, 'F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z', async (error, decoded) => {
+  jwt.verify(token, config.jwt.secretKey, async (error, decoded) => {
     if (error) {
       return res.status(401).json(AUTH_ERROR);
     }
