41a2ee23d31157515cc32e04116d70a0ce6de434
wonsikgo <wonsickgo@gmail.com>
2021-09-11 23:48:04 +0900

    Dwitter React 프론트 회원가입 로그인

--- Dwitter-project/client/src/service/tweet.js
+++ Dwitter-project/client/src/service/tweet.js
@@ -1,17 +1,21 @@
 export default class TweetService {
-  constructor(http) {
+  constructor(http, tokenStorage) {
     this.http = http;
+    this.tokenStorage = tokenStorage;
   }
+
   async getTweets(username) {
     let query = username ? `?username=${username}` : '';
     return this.http.fetch(`/tweets${query}`, {
       methos: 'GET',
+      headers: this.getHeaders(),
     });
   }
 
   async postTweet(text) {
     return this.http.fetch(`${this.baseURL}/tweets/`, {
       method: 'POST',
+      headers: this.getHeaders(),
       body: JSON.stringify({
         text,
         username: 'wonsick',
@@ -23,13 +27,22 @@ export default class TweetService {
   async deleteTweet(tweetId) {
     return this.http.fetch(`${this.baseURL}/tweets/${tweetId}`, {
       method: 'DELETE',
+      headers: this.getHeaders(),
     });
   }
 
   async updateTweet(tweetId, text) {
     return this.http.fetch(`${this.baseURL}/tweets/${tweetId}`, {
       method: 'PUT',
+      headers: this.getHeaders(),
       body: JSON.stringify({text}),
     });
+  }
+
+  getHeaders() {
+    const token = this.tokenStorage.getToken();
+    return {
+      Authorization: `Bearer ${token}`,
+    };
   }
 }
--- Dwitter-project/server/data/tweet.js
+++ Dwitter-project/server/data/tweet.js
@@ -2,49 +2,56 @@ let tweets = [
   {
     id: '1',
     text: '드림코딩 화이팅!',
-    createdAt: Date.now().toString(),
-    name: 'Bob',
-    username: 'bob',
+    createdAt: new Date().toString(),
+    userId: '1',
   },
   {
     id: '2',
     text: '드림코딩 !',
-    createdAt: Date.now().toString(),
-    name: 'Bob',
-    username: 'wonsik',
+    createdAt: new Date().toString(),
+    userId: '1',
   },
 ];
 
 export async function getAll() {
-  return tweets;
+  return Promise.all(
+    tweets.map(async (tweet) => {
+      const {username, name, url} = await userRepository.findById(tweet.userId);
+      return {...tweet, username, name, url};
+    })
+  );
 }
 
 export async function getAllByUsername(username) {
-  return tweets.filter((t) => t.username === username);
+  return getAll().then((tweets) => tweets.filter((tweet) => tweet.username === username));
 }
 
 export async function getById(id) {
-  return tweets.find((t) => t.id === id);
+  const found = tweets.find((tweet) => tweet.id === id);
+  if (!found) {
+    return null;
+  }
+  const {username, name, url} = await userRepository.findById(found.userId);
+  return {...found, username, name, url};
 }
 
-export async function create(text, name, username) {
+export async function create(text, userId) {
   const tweet = {
-    id: Date.now().toString(),
+    id: new Date().toString(),
     text,
     createdAt: new Date(),
-    name,
-    username,
+    userId,
   };
   tweets = [tweet, ...tweets];
-  return tweets;
+  return getById(tweet.id);
 }
 
 export async function update(id, text) {
-  const tweet = tweets.find((t) => t.id === id);
+  const tweet = tweets.find((tweet) => tweet.id === id);
   if (tweet) {
     tweet.text = text;
   }
-  return tweet;
+  return getById(tweet.id);
 }
 
 export async function remove(id) {
