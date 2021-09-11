a793486433c6f042f79b8e4a57a6690209126e82
wonsikgo <wonsickgo@gmail.com>
2021-09-12 01:27:06 +0900

    호스트 환경변수값으로 변경

--- Dwitter-project/server/app.js
+++ Dwitter-project/server/app.js
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import 'express-async-errors';
 import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
+import {config} from './config.js';
 
 const app = express();
 
@@ -25,4 +26,4 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-app.listen(8080);
+app.listen(config.host.port);
