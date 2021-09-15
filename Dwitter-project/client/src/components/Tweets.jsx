852d31fefd39c0244318d8e56d2b8d46525fedf8
wonsikgo <wonsickgo@gmail.com>
2021-09-15 23:44:22 +0900

    드위터 프론트 소켓 수정

--- Dwitter-project/client/src/components/Tweets.jsx
+++ Dwitter-project/client/src/components/Tweets.jsx
@@ -1,21 +1,24 @@
-import React, { memo, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, {memo, useEffect, useState} from 'react';
+import {useHistory} from 'react-router-dom';
 import Banner from './Banner';
 import NewTweetForm from './NewTweetForm';
 import TweetCard from './TweetCard';
-import { useAuth } from '../context/AuthContext';
+import {useAuth} from '../context/AuthContext';
 
-const Tweets = memo(({ tweetService, username, addable }) => {
+const Tweets = memo(({tweetService, username, addable}) => {
   const [tweets, setTweets] = useState([]);
   const [error, setError] = useState('');
   const history = useHistory();
-  const { user } = useAuth();
+  const {user} = useAuth();
 
   useEffect(() => {
     tweetService
       .getTweets(username)
       .then((tweets) => setTweets([...tweets]))
       .catch(onError);
+
+    const stopSync = tweetService.onSync((tweet) => onCreated(tweet));
+    return () => stopSync();
   }, [tweetService, username, user]);
 
   const onCreated = (tweet) => {
@@ -25,19 +28,13 @@ const Tweets = memo(({ tweetService, username, addable }) => {
   const onDelete = (tweetId) =>
     tweetService
       .deleteTweet(tweetId)
-      .then(() =>
-        setTweets((tweets) => tweets.filter((tweet) => tweet.id !== tweetId))
-      )
+      .then(() => setTweets((tweets) => tweets.filter((tweet) => tweet.id !== tweetId)))
       .catch((error) => setError(error.toString()));
 
   const onUpdate = (tweetId, text) =>
     tweetService
       .updateTweet(tweetId, text)
-      .then((updated) =>
-        setTweets((tweets) =>
-          tweets.map((item) => (item.id === updated.id ? updated : item))
-        )
-      )
+      .then((updated) => setTweets((tweets) => tweets.map((item) => (item.id === updated.id ? updated : item))))
       .catch((error) => error.toString());
 
   const onUsernameClick = (tweet) => history.push(`/${tweet.username}`);
@@ -51,13 +48,7 @@ const Tweets = memo(({ tweetService, username, addable }) => {
 
   return (
     <>
-      {addable && (
-        <NewTweetForm
-          tweetService={tweetService}
-          onError={onError}
-          onCreated={onCreated}
-        />
-      )}
+      {addable && <NewTweetForm tweetService={tweetService} onError={onError} onCreated={onCreated} />}
       {error && <Banner text={error} isAlert={true} transient={true} />}
       {tweets.length === 0 && <p className='tweets-empty'>No Tweets Yet</p>}
       <ul className='tweets'>
