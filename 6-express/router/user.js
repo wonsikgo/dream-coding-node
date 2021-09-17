0a477259fca068e249dae91527fefeabd7774133
wonsikgo <wonsickgo@gmail.com>
2021-09-17 23:59:13 +0900

    라우터 수정

--- 6-express/router/user.js
+++ 6-express/router/user.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
   res.status(201).send('GET: /users');
 });
 
+router.post('/', (req, res) => {
+  res.status(201).send('POST: /users');
+});
+
+router.put('/:id', (req, res) => {
+  res.status(201).send('PUT: /users/:id');
+});
+
+router.delete('/:id', (req, res) => {
+  res.status(201).send('DELETE: /users/:id');
+});
+
 export default router;
